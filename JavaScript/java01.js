d2eb1ad31a26845fe7edb2bfab153d7d82a4d78a
Emerson Gabriel <gabrilxbox@gmail.com>
2026-05-22 20:21:35 -0500

    aprendendo em java

---
+++ JavaScript/java01.js
@@ -0,0 +1,48 @@
+console.log("BOA NOITE HOJE E SEXTA FEIRAAAAAAAA")
+document.getElementById("check").innerHTML="aaaaaaaaaaaaaaaiiiiiiii"
+console.error("larem")
+console.warn("aiiiiiiipapapapapapaai")
+//variaveis
+var x = 10
+//variaveis mordenos
+let y = 15
+
+const z =  20
+console.log(x, y,z)
+
+//tipos de dados
+//texto
+const name = "Emerson Gabriel"
+console.log(name)
+console.log( typeof name)
+//numerico
+const shirtsQty = 4
+console.log(shirtsQty)
+console.log(typeof shirtsQty) 
+
+//numero decimal
+const decimal = 20.1
+
+console.log(decimal)
+console.log(typeof decimal)
+// BOOLEAN (true e false)
+const isAproved = false
+console.log(isAproved)
+console.log(typeof isAproved)
+//null
+let surname =null
+console.log(surname)
+console.log(typeof surname)
+//Array
+let languages = ["JavaScript, PHP, Python"]
+console.log(languages)
+console.log(typeof languages)
+// objetc
+let user = {email: "RagebaitAtolaCarro@gmail.com", password: "tentei", age: 18}
+console.log(user)
+console.log(typeof user)
+// metodo de string
+const fullName = "Emerson Gabriel Andrade Silva"
+console.log(fullName.length)
+console.log(typeof fullName)
+// metodo de string list
